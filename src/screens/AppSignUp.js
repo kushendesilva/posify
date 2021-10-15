9e0bcfabf0b131f628765cab5b006e3f548c61d8
Kushen De Silva <60555833+kushenthimira@users.noreply.github.com>
2021-10-15 05:52:22 +0530

    stable v1.2.1
    -added working signup page
    -added blue logo

--- src/screens/AppSignUp.js
+++ src/screens/AppSignUp.js
@@ -8,16 +8,23 @@ import {
   Image,
   TouchableWithoutFeedback,
 } from "react-native";
-import { Snackbar } from "react-native-paper";
-import { Icon, Button, Input, Modal, Card, Text } from "@ui-kitten/components";
+import { ToggleButton } from "react-native-paper";
+import { Icon, Button, Input, Text } from "@ui-kitten/components";
 import AppColors from "../configs/AppColors";
+import AppRenderIf from "../configs/AppRenderIf";
 import { firebase } from "../configs/Database";
 
 function AppSignUp({ navigation }) {
-  const LoginIcon = (props) => <Icon {...props} name="chevron-right-outline" />;
+  const LoginIcon = (props) => (
+    <Icon {...props} name="checkmark-circle-outline" />
+  );
+
+  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [secureTextEntry, setSecureTextEntry] = useState(true);
+  const [type, setType] = useState(null);
 
   const toggleSecureEntry = () => {
     setSecureTextEntry(!secureTextEntry);
@@ -29,23 +36,32 @@ function AppSignUp({ navigation }) {
     </TouchableWithoutFeedback>
   );
 
-  const onLoginPress = () => {
+  const onRegisterPress = () => {
+    if (password !== confirmPassword) {
+      alert("Passwords don't match.");
+      return;
+    }
+    if (type == null) {
+      alert("Select An Account Type.");
+      return;
+    }
     firebase
       .auth()
-      .signInWithEmailAndPassword(email.toLowerCase(), password)
+      .createUserWithEmailAndPassword(email, password)
       .then((response) => {
         const uid = response.user.uid;
+        //const itemID = response.doc.id;
+        const data = {
+          id: uid,
+          email,
+          fullName,
+          type,
+        };
+
         const usersRef = firebase.firestore().collection("users");
         usersRef
           .doc(uid)
-          .get()
-          .then((firestoreDocument) => {
-            if (!firestoreDocument.exists) {
-              alert("User does not exist anymore.");
-              return;
-            }
-            const user = firestoreDocument.data();
-          })
+          .set(data)
           .catch((error) => {
             alert(error);
           });
@@ -54,12 +70,16 @@ function AppSignUp({ navigation }) {
         alert(error);
       });
   };
+
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={AppColors.primary} barStyle="light-content" />
+      <StatusBar
+        backgroundColor={AppColors.background}
+        barStyle="dark-content"
+      />
       <View style={styles.header}>
         <Image
-          source={require("../assets/logo.png")}
+          source={require("../assets/icon.png")}
           style={styles.logo}
           resizeMode="stretch"
         />
@@ -68,7 +88,7 @@ function AppSignUp({ navigation }) {
         style={[
           styles.footer,
           {
-            backgroundColor: AppColors.background,
+            backgroundColor: AppColors.primary,
           },
         ]}
       >
@@ -79,44 +99,172 @@ function AppSignUp({ navigation }) {
             <Input
               style={{ marginHorizontal: "2%", marginVertical: "1%" }}
               size="large"
-              status="primary"
-              value={email}
+              status="control"
+              value={fullName}
               label="Name"
-              placeholder="Your Name"
-              onChangeText={(nextValue) => setEmail(nextValue)}
+              placeholder="Enter Your Name"
+              onChangeText={(nextValue) => setFullName(nextValue)}
             />
             <Input
               style={{ marginHorizontal: "2%", marginVertical: "1%" }}
               size="large"
-              status="primary"
+              status="control"
               value={email}
               label="Email"
-              placeholder="Your Email"
+              placeholder="Enter Your Email"
               onChangeText={(nextValue) => setEmail(nextValue)}
             />
+
             <Input
               style={{ marginHorizontal: "2%", marginVertical: "1%" }}
               size="large"
-              status="primary"
+              status="control"
               value={password}
               label="Password"
-              placeholder="Your Password"
+              placeholder="Enter A New Password"
               accessoryRight={renderIcon}
               secureTextEntry={secureTextEntry}
               onChangeText={(nextValue) => setPassword(nextValue)}
             />
+            <Input
+              style={{ marginHorizontal: "2%", marginVertical: "1%" }}
+              size="large"
+              status="control"
+              value={confirmPassword}
+              label="Confirm Password"
+              placeholder="Confirm Password"
+              accessoryRight={renderIcon}
+              secureTextEntry={secureTextEntry}
+              onChangeText={(nextValue) => setConfirmPassword(nextValue)}
+            />
+            <View
+              style={{
+                margin: "2%",
+              }}
+            >
+              <Text
+                style={{
+                  textTransform: "capitalize",
+                  textAlign: "left",
+                  marginBottom: "1%",
+                }}
+                appearance="alternative"
+                category="label"
+              >
+                Account Type
+              </Text>
+              <View
+                style={{
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  flexDirection: "row",
+                  backgroundColor: "#648afe",
+                  borderRadius: 5,
+                  padding: "2%",
+                  borderWidth: 0.3,
+                  borderColor: AppColors.background,
+                }}
+              >
+                {AppRenderIf(
+                  type != null,
+                  <>
+                    <Text
+                      style={{ textTransform: "capitalize", marginLeft: "3%" }}
+                      appearance="alternative"
+                      category="p1"
+                    >
+                      {type}
+                    </Text>
 
+                    <ToggleButton.Row
+                      onValueChange={(value) => setType(value)}
+                      value={type}
+                      style={{ marginRight: "3%" }}
+                    >
+                      {AppRenderIf(
+                        type == "store",
+                        <ToggleButton
+                          style={{ backgroundColor: AppColors.primary }}
+                          color={AppColors.background}
+                          icon="cart-outline"
+                          value="store"
+                        />
+                      )}
+                      {AppRenderIf(
+                        type == "supplier",
+                        <ToggleButton
+                          style={{ backgroundColor: AppColors.background }}
+                          color={AppColors.primary}
+                          icon="cart-outline"
+                          value="store"
+                        />
+                      )}
+                      {AppRenderIf(
+                        type == "store",
+                        <ToggleButton
+                          style={{ backgroundColor: AppColors.background }}
+                          color={AppColors.primary}
+                          icon="truck-outline"
+                          value="supplier"
+                        />
+                      )}
+                      {AppRenderIf(
+                        type == "supplier",
+                        <ToggleButton
+                          style={{ backgroundColor: AppColors.primary }}
+                          color={AppColors.background}
+                          icon="truck-outline"
+                          value="supplier"
+                        />
+                      )}
+                    </ToggleButton.Row>
+                  </>
+                )}
+                {AppRenderIf(
+                  type == null,
+                  <>
+                    <Text
+                      style={{ marginLeft: "3%" }}
+                      appearance="alternative"
+                      category="p1"
+                    >
+                      Select an Account Type
+                    </Text>
+
+                    <ToggleButton.Row
+                      onValueChange={(value) => setType(value)}
+                      value={type}
+                      style={{ marginRight: "3%" }}
+                    >
+                      <ToggleButton
+                        style={{ backgroundColor: AppColors.background }}
+                        color={AppColors.primary}
+                        icon="cart-outline"
+                        value="store"
+                      />
+                      <ToggleButton
+                        style={{ backgroundColor: AppColors.background }}
+                        color={AppColors.primary}
+                        icon="truck-outline"
+                        value="supplier"
+                      />
+                    </ToggleButton.Row>
+                  </>
+                )}
+              </View>
+            </View>
             <Button
-              status="primary"
+              status="control"
               accessoryRight={LoginIcon}
               style={styles.button}
-              onPress={() => onLoginPress()}
+              onPress={() => onRegisterPress()}
               size="large"
             >
               Sign Up
             </Button>
 
             <Button
+              status="control"
               appearance="ghost"
               onPress={() => navigation.navigate("LoginScreen")}
             >
@@ -132,12 +280,12 @@ function AppSignUp({ navigation }) {
 export default AppSignUp;
 
 const { height } = Dimensions.get("screen");
-const height_logo = height * 0.12;
+const height_logo = height * 0.1;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: AppColors.primary,
+    backgroundColor: AppColors.background,
   },
   header: {
     flex: 1,
@@ -145,8 +293,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   footer: {
-    flex: 1.5,
-    backgroundColor: "#fff",
+    flex: 3.5,
+    backgroundColor: AppColors.primary,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     paddingTop: "8%",
@@ -160,16 +308,16 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: "4%",
+    margin: "2%",
     marginTop: "5%",
-    marginHorizontal: "2%",
   },
   text: {
-    color: AppColors.primary,
+    color: AppColors.background,
     fontSize: 25,
     fontWeight: "bold",
     alignSelf: "center",
   },
   backdrop: {
-    backgroundColor: AppColors.background,
+    backgroundColor: AppColors.primary,
   },
 });
