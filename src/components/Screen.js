b67509b738f7326388c5dbe221116163a6015194
Kushen De Silva <60555833+kushenthimira@users.noreply.github.com>
2021-10-17 12:55:47 +0530

    release v2.1.0
    -fixed dark theme

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Layout, useTheme } from "@ui-kitten/components";
 import { StatusBar } from "react-native";
 
-function Screen({ children, color = "#FFFFFF" }) {
+function Screen({ children }) {
   const theme = useTheme();
   return (
-    <Layout style={{ flex: 1, backgroundColor: color }}>
+    <Layout style={{ flex: 1 }}>
       <StatusBar
         backgroundColor={theme["color-primary-default"]}
         barStyle="light-content"
